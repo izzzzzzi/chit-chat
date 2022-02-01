6f5171c1f1a535de6ae4a159605c28c767cfa0e9
Liz <liz@Lizui-MacBookPro.local>
2022-02-01 22:21:59 +0900

    try to receive refresh token and profile

---
+++ chit-chat/src/GoogleLoginButton.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import axios from 'axios';
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+
+export default function GoogleButton({ onGoogleLogin }){
+    // 로그인 성공 
+    const onSuccess = async(response) => {
+        // console.log(response)
+        const profile = response.getBasicProfile();
+
+        console.log(response)
+        // access_token : Google API 요청을 위해 보내는 토큰
+        // expires_in : access token 남은 시간(초)
+        // refresh_token : 새 액세스 토큰을 얻는 데 사용할 수 있는 토큰
+        const {tokenObj : { access_token, id_token, token_type, expires_in} } = response;
+
+        const googleId = profile.getId();
+        const googleEmail = profile.getEmail();
+
+        const data = {
+            googleId,
+            googleEmail,
+            access_token,
+            id_token,
+            token_type,
+            expires_in
+        }
+        // console.log(data)
+        axios.post('/login', data)
+        .then(console.log("success"))
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    const onFailure = (error) => {
+        console.log(error);
+    }
+
+    return(
+        <div>
+            <GoogleLogin
+                clientId={CLIENT_ID}
+                buttonText="Login With Google"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                accessType="offline"
+                responseType="token"
+                // responseType=""
+                prompt="consent"
+            />
+        </div>
+    )
+}
