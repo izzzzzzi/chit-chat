5099dfcf159aaad24185dd4a3922b9572415491d
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-02-02 15:35:22 +0900

    feat: Initialize basic chit-chat Spring Boot project

---
+++ chit-chat-login-api/src/test/java/chitchat/ChitChatLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package chitchat;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChitChatLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
