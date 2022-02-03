e425134932529faf365c8cd0a589ae96770a80d6
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-02-03 23:20:59 +0900

    feat: Initialize basic Oauth service(Google, Facebook, Kakao, Naver) with FE, BE

---
+++ chit-chat-backend/login-api/src/main/java/chitchat/oauth/exception/OAuthProviderMissMatchException.java
@@ -0,0 +1,8 @@
+package chitchat.oauth.exception;
+
+public class OAuthProviderMissMatchException extends RuntimeException {
+
+    public OAuthProviderMissMatchException(String message) {
+        super(message);
+    }
+}
