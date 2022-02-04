38e981cdbff6796990480b23cb415f03aa2248e5
Liz <liz@Lizui-MacBookPro.local>
2022-02-05 03:03:01 +0900

    create login modal & and social login layout

---
+++ chit-chat-frontend/src/component/Button.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import Button from '@mui/material/Button';
+import Modal from 'react-modal';
+import Login from "./Login";
+
+Modal.setAppElement("#root");
+
+export default function LoginBtn() {
+    const [isModalOpen, setIsOpen] = useState(false);
+
+    const toggleModal = () =>{
+        setIsOpen(!isModalOpen);
+    }
+
+    return (
+        <>
+        <Button onClick={toggleModal}>Login</Button>
+        <Modal isOpen={isModalOpen}>
+           <button onClick={toggleModal}>Close modal</button>
+           <Login/>
+        </Modal>
+        </>
+    );
+}
--- chit-chat-frontend/src/pages/Home.jsx
+++ chit-chat-frontend/src/pages/Home.jsx
@@ -1,18 +1,16 @@
 import logo from '../logo.png';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import GoogleLoginButton from '../component/GoogleLoginButton';
+import Button from '../component/Button';
 
 const Home = () => {
   return (
     <div>
       <h1>Main</h1>
       <img src={logo} className="App-logo" alt="logo" />
-      <Link to="/about">About</Link>
-      <Link to="/profiles/test1">test1</Link>
-      <Link to="/profiles/test2">test2</Link>
-      <Link to="/profiles/void">void</Link>
-      <GoogleLoginButton/>
+{/*       <Link to="/about">About</Link> */}
+{/*       <Link to="/profiles/test1">test1</Link> */}
+      <Button/>
     </div>
   )
 }
