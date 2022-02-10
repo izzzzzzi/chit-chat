e930d61b062cae7c71277a8ae631b8e09f737fd5
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-02-10 20:12:18 +0900

    chore: Delete not used files

--- chit-chat-frontend/src/index.js
+++ chit-chat-frontend/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import router from './router'
-import axios from 'axios'
-import store from './store'
-
-const BACKEND_PORT = process.env.BACKEND_PORT === null ? '' : `:${process.env.BACKEND_PORT}`
-const BACKEND_DOMAIN = window.location.hostname
-axios.defaults.baseURL = `http://${BACKEND_DOMAIN}${BACKEND_PORT}`
+import App from './app/App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
 );
+
+registerServiceWorker();
