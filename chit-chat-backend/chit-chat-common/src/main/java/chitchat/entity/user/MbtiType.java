d321a774fde2c21f6efa95cc039c381b1e4eed34
izzzzzzi <78688607+izzzzzzi@users.noreply.github.com>
2022-02-26 00:54:50 +0900

    [2, backend, feat]: create profile entity

---
+++ chit-chat-backend/chit-chat-common/src/main/java/chitchat/entity/user/MbtiType.java
@@ -0,0 +1,11 @@
+package chitchat.entity.user;
+
+import lombok.Getter;
+
+@Getter
+public enum MbtiType {
+    ISTJ, ISFJ, ISFP, ISTP,
+    INFJ, INTP, INTJ, INFP,
+    ESFP, ESTP, ESTJ, ESFJ,
+    ENFJ, ENFP, ENTP, ENTJ
+}
