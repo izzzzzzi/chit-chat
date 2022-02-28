e58a623eddd84fb84cf31d623e161ad03c401c2b
izzzzzzi <78688607+izzzzzzi@users.noreply.github.com>
2022-03-01 00:42:09 +0900

    [11, frontend, feat]: profile css

--- chit-chat-frontend/src/user/profile/Profile.js
+++ chit-chat-frontend/src/user/profile/Profile.js
@@ -3,23 +3,49 @@ import './Profile.css';
 import Emoji from './Emoji';
 import profilePicture from '../../img/ProfilePicture.png'
 import { MBTI_TYPE, ENNEAGRAM_TYPE } from '../../constants/index'
+import Layout from '../../components/Layout'
+import ProgressBar from '../../components/ProgressBar';
 
 export default function Profile(props) {
-    const [userInfo, setUserInfo] = useState(props.currentUser);
+    const userInfo = props.currentUser;
     const mbtiList = MBTI_TYPE.map((mbti, i) => (<li key={i}>{mbti}</li>));
     const enneagramList = ENNEAGRAM_TYPE.map((e, i) => (<li key={i}>{e}</li>));
 
+    const testData = [
+        { bgcolor: "rgb(75, 73, 73)", completed: 60 },
+        { bgcolor: "rgb(75, 73, 73)", completed: 30 },
+        { bgcolor: "rgb(75, 73, 73)", completed: 53 },
+    ];
+
     return (
-        <div className='profile-info'>
-            <div className='profile-avatar'>
-                <img 
-                    src={userInfo.profileImageUrl ? userInfo.profileImageUrl : profilePicture} 
-                    alt={userInfo.username}/>
-                <Emoji className="profile-name" label="alien" symbol="👽︎"/>
+        <Layout>
+                <div className='profile-avatar'>
+                    <img 
+                        src={userInfo.profileImageUrl ? userInfo.profileImageUrl : profilePicture} 
+                        alt={userInfo.username}/>
+                     <Emoji className="profile-name" label="alien" symbol="👽︎"/>
+                </div>
+            <div className='vote-text'>
+                <h2>Vote</h2>
             </div>
-            <div className='options'><ul>{mbtiList}</ul></div>
-            <div className='options'><ul>{enneagramList}</ul></div>
-        </div>
+                <div className='vote-container'>
+                    <div className='options'>
+                        <ul>
+                        {testData.map((item, idx) => (
+                        <li>INTP<ProgressBar className="progress-bar" key={idx} bgcolor={item.bgcolor} completed={item.completed} /></li>
+                        ))}
+                        </ul>
+                    </div>
+                    <div className='options'>
+                        <ul>
+                        {testData.map((item, idx) => (
+                        <li>1w9<ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} /></li>
+                        ))}
+                        </ul>
+                    </div>
+                </div>
+        </Layout>
+        
     )
 }
 
