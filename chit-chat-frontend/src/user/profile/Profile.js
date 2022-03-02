84a8cf6f1f47d53acb72bd28340acd863c081302
izzzzzzi <78688607+izzzzzzi@users.noreply.github.com>
2022-03-02 23:37:12 +0900

    [14, frontend, error]: voteModal

--- chit-chat-frontend/src/user/profile/Profile.js
+++ chit-chat-frontend/src/user/profile/Profile.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Profile.css';
 import Emoji from './Emoji';
 import profilePicture from '../../img/ProfilePicture.png'
 import { MBTI_TYPE, ENNEAGRAM_TYPE } from '../../constants/index'
 import Layout from '../../components/Layout'
 import ProgressBar from '../../components/ProgressBar';
+import ApiList from '../../api/ApiList';
 
 export default function Profile(props) {
-    const userDetail = props.currentUser;
-    const mbtiList = MBTI_TYPE.map((mbti, i) => (<li key={i}>{mbti}</li>));
-    const enneagramList = ENNEAGRAM_TYPE.map((e, i) => (<li key={i}>{e}</li>));
+    const [userDetail, setUserDetail] = useState({});
+    // const userDetail = props.currentUser;
+    const [mbtiCount, setMbtiCount] = useState([]);
+    const [enneagramCount, setenneagramCount] = useState([]);
+    // mbtiList = MBTI_TYPE.map((mbti, i) => (<li key={i}>{mbti}</li>));
+    // const enneagramList = ENNEAGRAM_TYPE.map((e, i) => (<li key={i}>{e}</li>));
+
+      useEffect(() => {
+        ApiList.getCurrentUser(res => {
+        setUserDetail(res.user);
+        const data = res.user.voteRecords
+        const mbtis = data[Object.keys(data)[0]];
+        const enneagrams = data[Object.keys(data)[1]];
+        setMbtiCount(mbtis);
+        setenneagramCount(enneagrams);
+        if (!res) {console.log(res)};
+        })
+        }, []);
 
     const testData = [
         { bgcolor: "rgb(75, 73, 73)", completed: 60 },
@@ -31,15 +47,15 @@ export default function Profile(props) {
                 <div className='vote-container'>
                     <div className='options'>
                         <ul>
-                        {testData.map((item, idx) => (
-                        <li>INTP<ProgressBar className="progress-bar" key={idx} bgcolor={item.bgcolor} completed={item.completed} /></li>
+                        {mbtiCount.map((item, idx) => (
+                        <li><ProgressBar className="progress-bar" key={idx} bgcolor={item.bgcolor} completed={item.completed} /></li>
                         ))}
                         </ul>
                     </div>
                     <div className='options'>
                         <ul>
-                        {testData.map((item, idx) => (
-                        <li>1w9<ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} /></li>
+                        {enneagramCount.map((item, idx) => (
+                        <li>{item}<ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} /></li>
                         ))}
                         </ul>
                     </div>
