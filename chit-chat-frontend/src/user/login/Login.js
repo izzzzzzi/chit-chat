99bd42c55e4ea4b2b16e9dc308725d3ff9573aa4
izzzzzzi <78688607+izzzzzzi@users.noreply.github.com>
2022-03-17 22:44:04 +0900

    [17, frontend, feat]: delete gitHub login button

--- chit-chat-frontend/src/user/login/Login.js
+++ chit-chat-frontend/src/user/login/Login.js
@@ -44,8 +44,6 @@ function SocialLogin() {
             <img src={googleLogo} alt="Google" /> Log in with Google</a>
         <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
             <img src={fbLogo} alt="Facebook" /> Log in with Facebook</a>
-        <a className="btn btn-block social-btn github" href={GITHUB_AUTH_URL}>
-            <img src={githubLogo} alt="Github" /> Log in with Github</a>
     </div>
     )
 }
