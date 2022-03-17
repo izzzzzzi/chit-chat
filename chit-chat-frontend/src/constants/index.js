807677480b2196a3fb808c3c794a649c8cd2fa7c
izzzzzzi <78688607+izzzzzzi@users.noreply.github.com>
2022-03-17 22:46:49 +0900

    [17, frontend, feat]: delete gitHub login resource

--- chit-chat-frontend/src/constants/index.js
+++ chit-chat-frontend/src/constants/index.js
@@ -7,7 +7,6 @@ export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_LOGIN_URL + '/oauth2/authorization/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_LOGIN_URL + '/oauth2/authorization/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const GITHUB_AUTH_URL = API_BASE_LOGIN_URL + '/oauth2/authorization/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
 
 export const ENNEAGRAM_TYPE = [
     'i don\'t know you',
--- chit-chat-frontend/src/user/login/Login.js
+++ chit-chat-frontend/src/user/login/Login.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
 import './Login.css';
-import { GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL, GITHUB_AUTH_URL } from '../../constants';
+import { GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL } from '../../constants';
 import { Redirect } from 'react-router-dom'
 import fbLogo from '../../img/fb-logo.png';
 import googleLogo from '../../img/google-logo.png';
-import githubLogo from '../../img/github-logo.png';
 import Alert from 'react-s-alert';
 
 export default function Login(props) {
