fd6114da61f28af9e3047b34e7e1ec39310562a1
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-03-19 20:19:07 +0900

    [20, frontend, feat]: Apply server setting to oauth url variable

--- chit-chat-frontend/src/constants/index.js
+++ chit-chat-frontend/src/constants/index.js
@@ -3,7 +3,7 @@ export const API_BASE_USER_URL = `http://${window.location.hostname}:8081`;
 export const ACCESS_TOKEN = 'accessToken';
 export const USER = 'user';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth/redirect'
+export const OAUTH2_REDIRECT_URI = `http://${window.location.hostname}http://localhost:3000/oauth/redirect`
 
 export const GOOGLE_AUTH_URL = API_BASE_LOGIN_URL + '/oauth2/authorization/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_LOGIN_URL + '/oauth2/authorization/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
