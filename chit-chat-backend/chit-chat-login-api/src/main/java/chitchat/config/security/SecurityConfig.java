4ff0f15d95e1f4c8be6a54455109a9a7a764fee8
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-06-05 13:29:04 +0900

    [2-5, backend, refactor]: Simplify nginx proxy server & Simplify URI

--- chit-chat-backend/chit-chat-login-api/src/main/java/chitchat/config/security/SecurityConfig.java
+++ chit-chat-backend/chit-chat-login-api/src/main/java/chitchat/config/security/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
                 .and()
                     .redirectionEndpoint()
-                    .baseUri("/*/oauth2/code/*")
+                    .baseUri("/oauth2/code/*")
                 .and()
                     .userInfoEndpoint()
                     .userService(oAuth2UserService)
