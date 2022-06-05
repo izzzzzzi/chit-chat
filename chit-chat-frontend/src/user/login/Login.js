a8d6f10c4704a1a1dc24bdb7d4d37526055d824b
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2022-06-05 13:45:01 +0900

    [2-6, frontend, feat]: Hide Facebook login button

--- chit-chat-frontend/src/user/login/Login.js
+++ chit-chat-frontend/src/user/login/Login.js
@@ -41,8 +41,8 @@ function SocialLogin() {
     <div className="social-login">
         <a className="btn btn-block social-btn google" href={GOOGLE_AUTH_URL}>
             <img src={googleLogo} alt="Google" /> Log in with Google</a>
-        <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
-            <img src={fbLogo} alt="Facebook" /> Log in with Facebook</a>
+        {/* <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
+            <img src={fbLogo} alt="Facebook" /> Log in with Facebook</a> */}
     </div>
     )
 }
